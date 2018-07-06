0525829d62e34de3184c82543d8bc7e90fab25e5
Minji Kwon <minjik95@naver.com>
2018-07-06 18:25:39 +0900

    Sorting locations

---
+++ src/kr/ac/ds/sort/LocationSort.java
@@ -0,0 +1,54 @@
+package kr.ac.ds.sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LocationSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int x = 0;
+		int y = 0;
+		Location temp;
+		Location[] location = new Location[t];
+		
+		for(int i = 0; i < t; i++) {
+			x = sc.nextInt();
+			y = sc.nextInt();
+			
+			location[i] = new Location(x, y);
+		}
+		
+		Arrays.sort(location);
+		
+		for(int i = 0; i < t; i++) {
+			System.out.println(location[i].x + " " + location[i].y);
+		}
+	}
+
+}
+
+class Location implements Comparable<Location>{
+	int x = 0;
+	int y = 0;
+	
+	Location(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int compareTo(Location l) {
+		// TODO Auto-generated method stub
+		if(this.x > l.x)
+			return 1;
+		else if(this.x < l.x)
+			return -1;
+		else {
+			if(this.y > l.y) return 1;
+			else return -1;
+		}
+	}
+}
